606e6b221d008e93328fbcbbd182c4b4ba68a359
stan <119288936+mosetf@users.noreply.github.com>
2024-01-26 19:10:43 +0300

    update :5

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,82 +1,42 @@
 #!/usr/bin/python3
-""" Script that reads stdin line by line and computes metrics
-
-    Input format:
-        <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
-            <status code> <file size>
-        (if the format is not this one, the line must be skipped)
-
-    format: <status code>: <number>
 """
-import sys
+Module that performs log parsing from stdin
+"""
+
 import re
+import sys
+counter = 0
+file_size = 0
+statusC_counter = {200: 0, 301: 0, 400: 0,
+                   401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
 
-def print_status(total_size, status_codes):
-    """ Print Status Function """
-    print(f"File size: {total_size}")
-    for code, count in status_codes.items():
-        if count > 0:
-            print(f"{code}: {count}")
-
-
-def match_input(input_line):
-    """ Check if input line match """
-    data = {'status_code': 0, 'file_size': 0}
-
-    pattern = re.compile(r'\b^[0-9]+.[0-9]+.[0-9]+.[0-9]+ - '
-                         r'\[[0-9]+-[0-9]+-[0-9]+ '
-                         r'[0-9]+:[0-9]+:[0-9]+.[0-9]+\] '
-                         r'"GET \/projects\/260 HTTP\/1.1" '
-                         r'(200|301|400|401|403|404|405|500) ([0-9]+)$\b')
-
-    match = pattern.search(input_line)
-    if not match:
-        return (None)
-
-    data['status_code'] = int(match.group(1))
-    data['file_size'] = int(match.group(2))
-    return (data)
-
-
-def main():
-    """ Main Function """
-    total_size = 0
-    status_codes = {
-                200: 0,
-                301: 0,
-                400: 0,
-                401: 0,
-                403: 0,
-                404: 0,
-                405: 0,
-                500: 0
-            }
-    counter = 0
-
-    try:
-        for line in sys.stdin:
-            data = match_input(line)
-            counter += 1
-
-            if not data:
-                continue
-
-            total_size += data['file_size']
-            status_code = data['status_code']
-
-            if not status_codes.get(status_code, None):
-                status_codes[status_code] = 1
-            else:
-                status_codes[status_code] += 1
-
-            if counter % 10 == 0:
-                print_status(total_size, status_codes)
-        print_status(total_size, status_codes)
-    except KeyboardInterrupt:
-        print_status(total_size, status_codes)
-        raise
+def printCodes(dict, file_s):
+    """Prints the status code and the number of times they appear"""
+    print("File size: {}".format(file_s))
+    for key in sorted(dict.keys()):
+        if statusC_counter[key] != 0:
+            print("{}: {}".format(key, dict[key]))
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        for line in sys.stdin:
+            split_string = re.split('- |"|"| " " ', str(line))
+            statusC_and_file_s = split_string[-1]
+            if counter != 0 and counter % 10 == 0:
+                printCodes(statusC_counter, file_size)
+            counter = counter + 1
+            try:
+                statusC = int(statusC_and_file_s.split()[0])
+                f_size = int(statusC_and_file_s.split()[1])
+                # print("Status Code {} size {}".format(statusC, f_size))
+                if statusC in statusC_counter:
+                    statusC_counter[statusC] += 1
+                file_size = file_size + f_size
+            except:
+                pass
+        printCodes(statusC_counter, file_size)
+    except KeyboardInterrupt:
+        printCodes(statusC_counter, file_size)
+        raise
