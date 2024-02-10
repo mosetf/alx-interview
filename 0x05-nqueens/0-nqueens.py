1411b854edebaab27465279cb31e75762f66eba1
mosetf <stanlym315@gmail.com>
2024-02-10 23:12:56 +0300

    Add isSafe function to check if a queen can be placed

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -13,4 +13,18 @@ def printSolution(board):
     print(queens)
 
 
+def isSafe(board, row, col):
+    """Checks if a queen can be placed on board[row][col]"""
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+
 
