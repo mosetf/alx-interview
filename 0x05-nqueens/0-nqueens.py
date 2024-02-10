405d2f822d7cbd645ca2598a2fae1582b15e9196
mosetf <stanlym315@gmail.com>
2024-02-10 23:13:50 +0300

    Add command line argument validation to nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -53,4 +53,12 @@ def solveNQ(n):
     return
 
 
-i
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        sys.exit(1)
+    n = int(sys.argv[1])
+    solveNQ(n)
