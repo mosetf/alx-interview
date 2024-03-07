5c49d3249aca7ec8a4dbca0b4aa4f1576b41972b
mosetf <stanlym315@gmail.com>
2024-03-07 22:31:11 +0300

    Refactor island_perimeter function to calculate the perimeter of an island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,15 +13,21 @@ def island_perimeter(grid):
 
     Returns:
         int: The perimeter of the island.
-
-    Example:
-        grid = [
-            [0, 1, 0, 0],
-            [1, 1, 1, 0],
-            [0, 1, 0, 0],
-            [1, 1, 0, 0]
-        ]
-        island_perimeter(grid)  # Output: 16
     """
-    
-    
+    p = 0
+    if type(grid) != list:
+        return 0
+    s = len(grid)
+    for j, row in enumerate(grid):
+        n = len(row)
+        for i, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                j == 0 or (len(grid[j - 1]) > i and grid[j - 1][i] == 0),
+                i == n - 1 or (n > i + 1 and row[i + 1] == 0),
+                j == s - 1 or (len(grid[j + 1]) > i and grid[j + 1][i] == 0),
+                i == 0 or row[i - 1] == 0,
+            )
+            p += sum(edges)
+        return p
