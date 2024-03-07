4ffc82f05773104c75b946c0914581f5f36520c6
mosetf <stanlym315@gmail.com>
2024-03-07 22:50:29 +0300

    Refactor island_perimeter function to improve readability and performance

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -15,20 +15,19 @@ def island_perimeter(grid):
     Returns:
         int: The perimeter of the island.
     """
-    p = 0
-    if type(grid) != list:
-        return 0
-    s = len(grid)
-    for j, row in enumerate(grid):
-        n = len(row)
-        for i, cell in enumerate(row):
-            if cell == 0:
-                continue
-            edges = (
-                j == 0 or (len(grid[j - 1]) > i and grid[j - 1][i] == 0),
-                i == n - 1 or (n > i + 1 and row[i + 1] == 0),
-                j == s - 1 or (len(grid[j + 1]) > i and grid[j + 1][i] == 0),
-                i == 0 or row[i - 1] == 0,
-            )
-            p += sum(edges)
-        return p
+    count = 0
+    row = len(grid)
+    col = len(grid[0]) if row else 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+
+            idx = [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)]
+            check = [1 if k[0] in range(row) and k[1] in range(col) else 0
+                     for k in idx]
+
+            if grid[i][j]:
+                count += sum([1 if not r or not grid[k[0]][k[1]] else 0
+                              for r, k in zip(check, idx)])
+
+    return (count)
